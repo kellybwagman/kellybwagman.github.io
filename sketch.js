c8f9d3748ab690f21d22fde5efbc9a8653780f16
KellyBuckley <kellybwagman@gmail.com>
2020-08-10 18:06:10 -0500

    try fix acceleration

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ var x, y, z;
 var rotationAngle = 0.0;
 
 function setup() {
+  //font = loadFont('assets/SourceSansPro-Regular.otf');
   createCanvas(windowWidth, windowHeight, WEBGL);
   background(255);
   r = 255;
@@ -13,27 +14,41 @@ function setup() {
   y = 0;
   z = 0;
   frameRate(30);
+  
+  //textFont(font);
+  //textSize(20);
+  //textAlign(RIGHT);
 }
 
 function draw() {
   lights();
   
-  if (mouseIsPressed == true) {
-    x = (-width/2) + pmouseX;
-    y = (-height/2) + pmouseY;
-  }
+  //if (mouseIsPressed == true) {
+  //  x = (-width/2) + pmouseX;
+  //  y = (-height/2) + pmouseY;
+  //}
   
-  translate(x, y, z);
+  //translate(x, y, z);
   
-  rotateX(rotationAngle);
-  rotateY(rotationAngle);
-  rotateZ(rotationAngle);
-  rotationAngle = rotationAngle + 0.02;
+  //rotateX(rotationAngle);
+  //rotateY(rotationAngle);
+  //rotateZ(rotationAngle);
+  //rotationAngle = rotationAngle + 0.02;
   fill(r, g, b);
   
-  translate(100*accelerationX, 100*accelerationY, 100*accelerationZ);
-  //rotateX(accelerationX * 0.02);
-  //rotateY(accelerationY * 0.02);
+  //fill(50);
+  //text(accelerationX, -100, -100);
+  
+  if (accelerationX != 0)
+  {
+    print('x = ' + accelerationX);
+    print('y = ' + accelerationY);
+    print('z = ' + accelerationZ);
+  }
+  
+  translate(x + (10*accelerationX), y + (10*accelerationY), z + (10*accelerationZ));
+  rotateX(accelerationX * 0.02);
+  rotateY(accelerationY * 0.02);
   
   box(100, 100, 100);
   
