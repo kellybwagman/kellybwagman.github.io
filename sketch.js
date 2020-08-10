6b5c735f8a148e197e639f002a3b84927e710cf8
KellyBuckley <kellybwagman@gmail.com>
2020-08-10 12:02:22 -0500

    testing tilt

--- sketch.js
+++ sketch.js
@@ -3,14 +3,14 @@ var x, y, z;
 var rotationAngle = 0.0;
 
 function setup() {
-  createCanvas(400, 400, WEBGL);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   background(255);
   r = 255;
   g = 0;
   b = 255;
   i = 1;
-  x = width/2;
-  y = height/2;
+  x = 0;
+  y = 0;
   z = 0;
   frameRate(30);
 }
@@ -19,30 +19,34 @@ function draw() {
   lights();
   
   if (mouseIsPressed == true) {
-    x = mouseX;
-    y = mouseY;
+    x = (-width/2) + pmouseX;
+    y = (-height/2) + pmouseY;
   }
   
   translate(x, y, z);
-  rotateX(rotationAngle);
-  rotateY(rotationAngle);
-  rotateZ(rotationAngle);
-  rotationAngle = rotationAngle + 0.02;
+  
+  //rotateX(rotationAngle);
+  //rotateY(rotationAngle);
+  //rotateZ(rotationAngle);
+  //rotationAngle = rotationAngle + 0.02;
   fill(r, g, b);
-
-  translate(100*accelerationX, 100*accelerationY, 100*accelerationZ)
-
-  box(100);
   
-  x = x + random(-0.2, 0.2);
-  y = y + random(-0.2, 0.2);
-  z = z + random(-0.2, 0.2);
+  //translate(100*accelerationX, 100*accelerationY, 100*accelerationZ);
+  rotateX(accelerationX * 0.02);
+  rotateY(accelerationY * 0.02);
   
-  if (i % 20 == 0) {
+  box(100, 100, 100);
+  
+  //x = x + random(-0.2, 0.2);
+  //y = y + random(-0.2, 0.2);
+  //z = z + random(-0.2, 0.2);
+  
+  if (i % 40 == 0) {
     r = int(random(255));
     g = int(random(255));
     b = int(random(255));
   }
   
   i++;
+  
 }
